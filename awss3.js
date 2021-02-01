963eb7f45734a9705345cb96bb063eef9a06c08c
aozaydin <78221826+aozaydin@users.noreply.github.com>
2021-02-01 22:12:21 +0300

    Delete awss3.js

--- awss3.js
+++
@@ -1,9 +0,0 @@
-  var bucketName = 'nilhediye';
-  var bucketRegion = 'eu-central-1';
-  var IdentityPoolId = 'eu-central-1:350fb4b8-a162-4e28-9a9c-e361f3703a8c';
-  AWS.config.update({
-    region: bucketRegion,
-    credentials: new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: IdentityPoolId
-    })
-  });
